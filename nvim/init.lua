76df3fa40525abd9636fb905ad7d1a6d1e1082c1
v4ld1 <techv4ld1@gmail.com>
2025-09-15 12:53:34 +0200

    Nvim changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,3 +29,42 @@ vim.opt.virtualedit = "block"
 
 -- Sincroniza el portapapeles del sistema con el de Nvim
 vim.opt.clipboard = "unnamedplus"
+
+-- Dividira la pantalla si cambio algo en masa en el fichero
+-- por ejemplo si hago :%s/vim/nvim esto cambia todas las instancias de 
+--     vim por nvim y vere como queda este cambio en una pantalla abajo
+vim.opt.inccommand = "split"
+
+-- Nvim es case-sensitive con los comandos
+-- Esta opción me permite ignorar las mayusculas para autocompletar los comandos
+--     con Tab
+vim.opt.ignorecase = true
+
+vim.opt.termguicolors = true
+
+
+-- Set up lazy.nvim
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({ 
+      "git", 
+      "clone", 
+      "--filter=blob:none", 
+      "https://github.com/folke/lazy.nvim.git", 
+      "--branch=stable", 
+      lazypath 
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {
+        "rebelot/kanagawa.nvim",
+
+        config = function()
+            vim.cmd.colorscheme("kanagawa-wave")
+        end,
+    },
+})
+
